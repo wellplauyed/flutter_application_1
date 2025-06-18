295124e030494b6e27a1718591e4b2b50bf562a1
Neil Agosila <neilagosila9@gmail.com>
2025-06-18 17:15:35 +0800

    Update name to 'Neil agosilanq'

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Neil'),
+            const Text('Neil agosilanq'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
